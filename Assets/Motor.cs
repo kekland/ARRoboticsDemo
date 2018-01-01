da7b40572167df53334d46bbffcc84f1ae169df0
kekland <kk.erzhan@gmail.com>
2018-01-01 15:20:56 +0600

    First commit

---
+++ Assets/Motor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject attachedShaft;
+	private Rigidbody shaftRigidbody;
+	void Start () {
+		shaftRigidbody = attachedShaft.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		shaftRigidbody.AddRelativeTorque(new Vector3(0f, 0f, 1f), ForceMode.Force);
+	}
+}
